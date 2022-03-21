5c1955a378c8ba9618d390a01b30d6b94c4cd15a
Emmanuel Di Pretoro <emmanuel.dipretoro@kikirpa.be>
2022-03-21 14:48:11 +0100

    Using specifif envvars for fqdn and siteurl

--- init.d/03-doi-set.sh
+++ init.d/03-doi-set.sh
@@ -7,8 +7,8 @@ if [ "${doi_authority}" ]; then
       curl -X DELETE http://localhost:8080/api/admin/settings/:DoiProvider
       asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.handlenet.admcredfile\=${doi_handle_admcredfile}"
       asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.handlenet.index\=${doi_handle_index}"
-      asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.fqdn\=${hostname}"
-      asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.siteUrl\=http\\:\/\/${hostname}"
+      asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.fqdn\=${fqdn}"
+      asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.siteUrl\=${siteurl}"
       curl -X PUT -d ${doi_protocol} http://localhost:8080/api/admin/settings/:Protocol
       curl -X PUT -d ${doi_authority} http://localhost:8080/api/admin/settings/:Authority
       curl -X PUT -d ${doi_handle_handleauthhandle} http://localhost:8080/api/admin/settings/:HandleAuthHandle
